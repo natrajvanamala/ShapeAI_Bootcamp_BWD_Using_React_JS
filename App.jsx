69f0becd07b494aa1787f49ddcae8003a057f78a
Natraj Vanamala <natrajvanamalapro@gmail.com>
2021-06-26 17:31:47 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Info from "../components/Info";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Java Script and React.JS</h1>
+      <p>A Basic Boot Camp On HTML, CSS, JAVA SCRIPT AND REACT JS</p>
+    </div>
+  );
+}
+
+export default Info;
